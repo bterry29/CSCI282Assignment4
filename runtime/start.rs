9c6116aae6b0e98c34d217ae11a243aad69e7614
bterry29 <brndntrry@gmail.com>
2026-04-06 19:32:28 -0500

    Update star.rs

--- runtime/start.rs
+++ runtime/start.rs
@@ -1,6 +1,25 @@
 // runtime/start.rs
 // This file provides the entry point for compiled programs
-
+#[no_mangle]
+pub extern "C" fn snek_error(errcode:i64){
+    if errcode == 1{
+        eprintln!("invalid argument");
+    }else if errcode == 2{
+        eprintln!("overflow");
+    }
+    std::process::exit(1);
+}
+#[no_mangle]
+pub extern "C" fn snek_print(val: i64) -> i64 {
+    if val & 1 == 0{
+        println!("{}", val >> 1);
+    }else if val == 3{
+        println!("true");
+    }else if val == 1{
+        println!("false");
+    }
+    val
+}
 #[link(name = "our_code")]
 extern "C" {
     // The \x01 here is an undocumented feature of LLVM that ensures
@@ -13,5 +32,5 @@ fn main() {
     let i: i64 = unsafe {
         our_code_starts_here()
     };
-    println!("{i}");
+    snek_print(i);
 }
